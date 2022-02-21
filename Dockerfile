d67135baf32259d501f58418252d1ae2e2e59e52
Hannes Braun <hannesbraun@mail.de>
2022-02-21 18:41:59 +0100

    First working version

--- Dockerfile
+++ Dockerfile
@@ -4,23 +4,36 @@ EXPOSE 8000/tcp
 
 RUN apt-get update
 ARG DEBIAN_FRONTEND=noninteractive
-RUN apt-get install -yq postgresql apache2 libpq-dev zlib1g-dev libmysqlclient-dev libsasl2-dev libssl-dev swig libapache2-mod-xsendfile libapache2-mod-wsgi-py3 openjdk-8-jdk junit junit4 dejagnu r-base git-core python3-setuptools python3-psycopg2 python3-virtualenv python3-pip sudo
+RUN apt-get install -yq postgresql apache2 apache2-dev libpq-dev zlib1g-dev libmysqlclient-dev libsasl2-dev libssl-dev libffi-dev swig libapache2-mod-xsendfile libapache2-mod-wsgi-py3 openjdk-8-jdk junit junit4 dejagnu r-base git-core python3-setuptools python3-psycopg2 python3-virtualenv python3-pip sudo
 RUN useradd -m praktomat && adduser praktomat sudo
-RUN useradd -m tester && adduser tester sudo
+RUN useradd -m tester && adduser tester sudo && adduser tester praktomat
 COPY sudoers /etc/sudoers
 RUN chmod 440 /etc/sudoers
 
 USER postgres
-RUN /etc/init.d/postgresql start && createuser -DRS praktomat && createdb -O praktomat praktomat_default
+RUN /etc/init.d/postgresql start && createuser -DRS praktomat && createdb --encoding UTF8 -O praktomat praktomat_default && createdb --encoding UTF8 -O praktomat praktomat_hso
 
 USER praktomat
 WORKDIR /home/praktomat
-RUN pip3 install --upgrade pip && pip3 install -U pip virtualenv setuptools wheel urllib3[secure]
-RUN git clone --recursive git://github.com/KITPraktomatTeam/Praktomat.git && cd Praktomat && git checkout 7642e068a6b5ff60039528bd1ef8804d2ad30ceb
-RUN pip3 install -r Praktomat/requirements.txt
+RUN python3 -m pip install --upgrade pip && python3 -m pip install -U pip virtualenv setuptools wheel urllib3[secure]
+RUN git clone --recursive git://github.com/ifrh/Praktomat.git && cd Praktomat && git checkout eec5679a3c7de32f35b052a9acd23dce975f0615
+# TODO change back to KITPraktomatTeam once the PR is merged
+RUN python3 -m pip install -r Praktomat/requirements.txt
 COPY local.py Praktomat/src/settings/local.py
-ADD https://github.com/jplag/jplag/releases/download/v2.12.1-SNAPSHOT/jplag-2.12.1-SNAPSHOT-jar-with-dependencies.jar /srv/praktomat/contrib/jplag.jar
-ADD https://github.com/checkstyle/checkstyle/releases/download/checkstyle-5.8/checkstyle-5.8-all.jar /srv/praktomat/contrib/checkstyle.jar
-RUN sudo /etc/init.d/postgresql start && mkdir PraktomatSupport && python3 Praktomat/src/manage-local.py collectstatic --noinput --link && python3 Praktomat/src/manage-local.py migrate --noinput
+ADD https://github.com/jplag/jplag/releases/download/v2.12.1-SNAPSHOT/jplag-2.12.1-SNAPSHOT-jar-with-dependencies.jar /opt/praktomat-addons/jplag.jar
+ADD https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.14/checkstyle-8.14-all.jar /opt/praktomat-addons/checkstyle-8.14-all.jar
+RUN sudo chown -R praktomat:praktomat /opt/praktomat-addons
+COPY apache_praktomat_wsgi.conf Praktomat/documentation/apache_praktomat_wsgi.conf
+RUN printf "\nInclude /home/praktomat/Praktomat/documentation/apache_praktomat_wsgi.conf\nUse Praktomat hso /home/praktomat/\n" | sudo tee -a /etc/apache2/apache2.conf && sudo a2enmod macro
+RUN mkdir debug-data
+RUN mkdir work-data
+RUN mkdir test-data
+RUN sudo mkdir -p /srv/praktomat/sandbox
+RUN sudo chown praktomat:praktomat /srv/praktomat/sandbox
+RUN sudo chown praktomat:tester Praktomat/src/checker/scripts/java
+RUN sudo chown praktomat:tester Praktomat/src/checker/scripts/javac
+RUN sudo chmod u+x,g+x,o-x Praktomat/src/checker/scripts/java
+RUN sudo chmod u+x,g+x,o-x Praktomat/src/checker/scripts/javac
+RUN sudo /etc/init.d/postgresql start && python3 Praktomat/src/manage-local.py collectstatic --noinput --link && python3 Praktomat/src/manage-local.py migrate --noinput
 
 ENTRYPOINT sudo /etc/init.d/postgresql start && python3 Praktomat/src/manage-local.py runserver --insecure 0.0.0.0:8000
--- local.py
+++ local.py
@@ -1,11 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 # Settings for deployment
 
-from os.path import join, dirname
+from os.path import join, dirname, basename
+import re
 
 PRAKTOMAT_PATH = dirname(dirname(dirname(__file__)))
-PRAKTOMAT_ID = "aki-java"
+
+PRAKTOMAT_ID = "hso"
 
 SITE_NAME = "Programmierung mit Java (Wintersemester 2021/2022)"
+MIRROR = False
+
+USING_ISABELLE = False
 
 # The URL where this site is reachable. 'http://localhost:8000/' in case of the
 # development server.
@@ -15,27 +23,48 @@ BASE_PATH = '/'
 ALLOWED_HOSTS = [ '*', ]
 
 # URL to use when referring to static files.
-STATIC_URL = BASE_PATH + 'static/'
+# STATIC_URL = BASE_PATH + 'static/'
+# STATIC_ROOT = join(dirname(PRAKTOMAT_PATH), "static")
 
-STATIC_ROOT = join(dirname(PRAKTOMAT_PATH), "static")
+
+# STATIC_URL now defined in settings/defaults.py
+# STATIC_ROOT now defined in settings/defaults.py
 
 TEST_MAXLOGSIZE=512
 
 TEST_MAXFILESIZE=512
 
-TEST_TIMEOUT=300
-TEST_MAXMEM=200
+TEST_TIMEOUT=180
 
-# Rating overview needs one POST parameter per student
-# and the default value (1000) might be too low
-DATA_UPLOAD_MAX_NUMBER_FIELDS = 2000
+if "cram" in PRAKTOMAT_ID:
+    TEST_TIMEOUT=600
+    TEST_MAXMEM=200
+
+if "birap" in PRAKTOMAT_ID:
+    TEST_TIMEOUT=600
+
+if "tba" in PRAKTOMAT_ID:
+    TEST_TIMEOUT=600
+
+if "Programmieren" in SITE_NAME or "Programmierung" in SITE_NAME:
+    # Rating overview needs one POST parameter per student
+    # and the default value (1000) might be too low
+    DATA_UPLOAD_MAX_NUMBER_FIELDS = 2000
+    TEST_TIMEOUT=600
 
 # Absolute path to the directory that shall hold all uploaded files as well as
 # files created at runtime.
 
-UPLOAD_ROOT = join(PRAKTOMAT_PATH, "../PraktomatSupport/")
+# Example: "/home/media/media.lawrence.com/"
+UPLOAD_ROOT = join(dirname(PRAKTOMAT_PATH), "work-data/")
 
-SANDBOX_DIR = '/srv/praktomat/sandbox/'
+
+# SANDBOX_DIR now defined in settings/defaults.py
+
+#if MIRROR:
+#    SANDBOX_DIR = join('/srv/praktomat/sandbox_Mirror/', PRAKTOMAT_ID)
+#else:
+#    SANDBOX_DIR = join('/srv/praktomat/sandbox/', PRAKTOMAT_ID)
 
 ADMINS = [
   ('Hannes Braun', 'hannes.braun@hs-offenburg.de')
@@ -44,42 +73,102 @@ ADMINS = [
 SERVER_EMAIL = 'praktomat@i44vm3.info.uni-karlsruhe.de'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "localhost"
-EMAIL_PORT = 25
+#if MIRROR:
+#    EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+#    EMAIL_FILE_PATH = join(UPLOAD_ROOT, "sent-mails")
+#else:
+#    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#    EMAIL_HOST = "localhost"
+#    EMAIL_PORT = 25
+EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
 
 DEFAULT_FROM_EMAIL = "praktomat@ipd.info.uni-karlsruhe.de"
 
-DEBUG = False
+DEBUG = MIRROR
+
+#DATABASES = {
+#    'default': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+#            'NAME':   UPLOAD_ROOT+'/Database'+PRAKTOMAT_ID,
+#    }
+#}
 
 DATABASES = {
     'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME':   "praktomat_default",
+            'NAME':   'praktomat_'+PRAKTOMAT_ID,
+            'USER':   'praktomat',
+            'PASSWORD':   '',
     }
 }
+
+# on linux command line  create database and databaseuser
+#sudo -u postgres -P <db_user>
+#sudo -u postgres -P praktomat
+#
+#sudo -u postgres createdb -O <db_user> <db_name>
+#sudo -u postgres createdb -O praktomat praktomat_2017s
+
+# SECRET_KEY gets generated via defaults.py
 
 # Private key used to sign uploded solution files in submission confirmation email
 PRIVATE_KEY = '/srv/praktomat/mailsign/signer_key.pem'
 CERTIFICATE = '/srv/praktomat/mailsign/signer.pem'
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
 # Enable Shibboleth:
-SHIB_ENABLED = True
-REGISTRATION_POSSIBLE = False
+SHIB_ENABLED = False
+
+# Set this to False to disable registration via the website, e.g. when Single Sign On is used
+REGISTRATION_POSSIBLE = True
+
+
+# If you use shibboleth identitiy provider, please have a look into defaults.py
+# to see how to overwrite SHIB_ATTRIBUTE_MAP , SHIB_USERNAME , SHIB_PROVIDER
+
+
+
 
 SYSADMIN_MOTD_URL = "about:blank"
 
 # Use a dedicated user to test submissions
 USEPRAKTOMATTESTER = True
 
+# It is recomendet to use DOCKER and not a tester account
+# for using Docker from https://github.com/nomeata/safe-docker
 # Use docker to test submission
 USESAFEDOCKER = False
 
-# Various extra files and versions
-CHECKSTYLEALLJAR = '/srv/praktomat/contrib/checkstyle.jar'
-JPLAGJAR = '/srv/praktomat/contrib/jplag.jar'
-#JAVA_BINARY = 'javac-sun-1.7'
-#JVM = 'java-sun-1.7'
+# Linux User "tester" and Usergroup "praktomat"
+# Enable to run all scripts (checker) as the unix user 'tester'.
+# Therefore put 'tester' as well as the Apache user '_www' (and your development user account)
+# into a new group called "praktomat".
+
+# Also edit your sudoers file with "sudo visudo -f /etc/sudoers.d/praktomat-tester".
+# Add the following lines to the end of the file to allow the execution of
+# commands with the user 'tester' without requiring a password:
+# "_www		ALL=(tester) NOPASSWD: ALL"
+# "developer	ALL=(tester) NOPASSWD: ALL"
+
+# Add the following lines to the end of the file
+# to allow user Praktomat the execution of scriptfile  safe-docker  without requiring a password:
+# "praktomat	ALL= NOPASSWD: /usr/local/bin/safe-docker"
+
+# If you want to switch between "testuser" and "Docker"
+# use "sudo visudo -f /etc/sudoers.d/praktomat-tester"
+# "_www		ALL=(tester) NOPASSWD: ALL"
+# "developer	ALL=(tester) NOPASSWD: ALL"
+# "praktomat 	ALL=(tester) NOPASSWD: ALL, NOPASSWD: /usr/local/bin/safe-docker"
+#
+# be sure that you change file permission
+# sudo chown praktomat:tester praktomat/src/checker/scripts/java
+# sudo chown praktomat:tester praktomat/src/checker/scripts/javac
+# sudo chmod u+x,g+x,o-x praktomat/src/checker/scripts/java
+# sudo chmod u+x,g+x,o-x praktomat/src/checker/scripts/javac
+
+
+
 
 # Does Apache use "mod_xsendfile" version 1.0?
 # If you use "libapache2-mod-xsendfile", this flag needs to be set to False
@@ -87,6 +176,20 @@ MOD_XSENDFILE_V1_0 = False
 
 # Our VM has 4 cores, so lets try to use them
 NUMBER_OF_TASKS_TO_BE_CHECKED_IN_PARALLEL = 6
+# But not with Isabelle, which is memory bound
+if USING_ISABELLE:
+    NUMBER_OF_TASKS_TO_BE_CHECKED_IN_PARALLEL = 1
+
+
+
+# Various extra files and versions
+
+JPLAGJAR = '/opt/praktomat-addons/jplag.jar'
+
+CHECKSTYLEALLJAR = '/opt/praktomat-addons/checkstyle-8.14-all.jar'
+
+LANG = "en_US.UTF-8"
+LANGUAGE = "en_US:en"
 
 # Finally load defaults for missing settings.
 from . import defaults
