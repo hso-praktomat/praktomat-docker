a8126c8167a84a8c187010959877de8d49f5b231
Hannes Braun <hannesbraun@mail.de>
2022-03-30 12:53:56 +0200

    Remove praktomat_default database as it is not used

--- postgresql/init.sh
+++ postgresql/init.sh
@@ -2,5 +2,4 @@
 
 createuser -DRS praktomat
 psql -c "ALTER USER praktomat WITH ENCRYPTED PASSWORD 'praktomat_password';"
-createdb --encoding UTF8 -O praktomat praktomat_default
 createdb --encoding UTF8 -O praktomat praktomat_${COMPOSE_PROJECT_NAME}
