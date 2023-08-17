4147c7f3f433dbaec53db2cc828b472ed70fa9b7
Stefan Wehr (hso-progcheck) <progcheck@progcheck.emi.hs-offenburg.de>
2023-08-17 09:47:00 +0200

    fix backup script to detect non-existing containers

--- tools/backup.py
+++ tools/backup.py
@@ -26,6 +26,7 @@ def dumpPostgres(container, dbName):
     today = date.today().isoformat()
     basename = pjoin(pgBackupDir, f'pg_dump_{container}_{dbName}_{today}')
     backupName = freeFilename(basename, '.gz')
+    run(f"docker exec {container} true")
     cmd = f"docker exec {container} pg_dump -U postgres {dbName} | gzip > {backupName}"
     info(cmd)
     run(cmd)
